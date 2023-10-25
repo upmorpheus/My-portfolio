1a7a4de3cc6199021c423f2132a73a646bbca34d
Morpheus <mangoo.pay@gmail.com>
2023-10-25 12:05:50 -0700

    feat: add the Experience page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import {
   RouterProvider
 } from 'react-router-dom';
 import Home from "./views";
-import EBooks from "./views/Ebooks";
 import Management from "./views/Management";
 import Register from "./views/auth/Register";
 import Login from "./views/auth/Login";
@@ -18,7 +17,6 @@ const publicRouter = createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={<Home />} />,
     <Route path='login' element={<Login />}/>,
     <Route path='register' element={<Register />} />,
-    <Route path='e-books' element={<EBooks />} />,
     <Route path='management' element={<Management />} />,
   ]
 ));
