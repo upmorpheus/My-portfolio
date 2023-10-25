46ee87031ff2b158c0282b0e421e00f2165c43b9
Morpheus <mangoo.pay@gmail.com>
2023-10-25 12:28:35 -0700

    feat: View style design

--- src/views/index.js
+++ src/views/index.js
@@ -2,12 +2,20 @@ import React from "react";
 import Experience from "./Experience";
 import AboutMe from "./AboutMe";
 import Services from "./Services";
+import Counter from "./Counter";
+import Portfolio from "./Portfolio";
+import Testimonials from "./Testimonials";
+import Contact from "./Contact";
 
 const Home = () => (
     <div className="home">
         <AboutMe />
         <Experience />
         <Services />
+        <Counter />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
     </div>
 );
 
